70766fbe756d658858661ebe7d7a65a500599a80
Nil <nilarmengol@gmail.com>
2019-10-08 22:03:32 +0700

    grid of two columns with image in left one

--- src/components/Homepage.js
+++ src/components/Homepage.js
@@ -4,16 +4,36 @@ import "../styles/general.css";
 class Homepage extends React.Component {
   render() {
     return (
-      <div class="topnav">
-        <a href="#news">Contactar</a>
-        <a href="#contact">Servicios Nutrición</a>
-        <a href="#about">Testimonios</a>
-        <a href="#about">Quién soy</a>
-        <a href="#about">Cómo funciona</a>
-        <a className="logo" href="#about">
-          Renata Scassi
-        </a>
-      </div>
+      <body>
+        <div class="topnav">
+          <a href="#news">Contactar</a>
+          <a href="#contact">Servicios Nutrición</a>
+          <a href="#about">Testimonios</a>
+          <a href="#about">Quién soy</a>
+          <a href="#about">Cómo funciona</a>
+          <a className="logo" href="#about">
+            Renata Scassi
+          </a>
+        </div>
+        <div className="grid two medium">
+          <div class="background"></div>
+          <div class="column">
+            <div>
+              <h2>SOY RENATA SCASSI</h2>
+              <h6>
+                Nutricionista Personal Licenciada en Dietética y Nutrición
+              </h6>
+            </div>
+            <div className="highlight">
+              <h1>
+                ¿Quieres perder grasa y recuperar tu forma física en{" "}
+                <u>90 días</u>?
+              </h1>
+              <h1>¡Te ayudo a conseguirlo!</h1>
+            </div>
+          </div>
+        </div>
+      </body>
     );
   }
 }
