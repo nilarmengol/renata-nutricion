2314f4e6bdf6feecd4e8a6f4830635f4e4a0d5c2
Nil <nilarmengol@gmail.com>
2019-10-10 18:19:49 +0700

    added grid

--- src/components/Homepage.js
+++ src/components/Homepage.js
@@ -18,20 +18,57 @@ class Homepage extends React.Component {
         <div className="grid two medium">
           <div class="background"></div>
           <div class="column">
-            <div>
-              <h2>SOY RENATA SCASSI</h2>
-              <h6>
-                Nutricionista Personal Licenciada en Dietética y Nutrición
-              </h6>
+            <div class="description">
+              <div>
+                <h2>SOY RENATA SCASSI</h2>
+                <h6>
+                  Nutricionista Personal Licenciada en Dietética y Nutrición
+                </h6>
+              </div>
+              <div className="highlight">
+                <h1>
+                  ¿Quieres perder grasa y recuperar tu forma física en{" "}
+                  <u>90 días</u>?
+                </h1>
+                <h1>¡Te ayudo a conseguirlo!</h1>
+              </div>
             </div>
-            <div className="highlight">
-              <h1>
-                ¿Quieres perder grasa y recuperar tu forma física en{" "}
-                <u>90 días</u>?
-              </h1>
-              <h1>¡Te ayudo a conseguirlo!</h1>
+            <div className="grid two">
+              <div className="smallColumn grey">
+                <h3>Transforma Tu Cuerpo En 90 Días</h3>
+                <p>
+                  Te muestro QUE y COMO debes hacerlo exactamente para conseguir
+                  resultados. Prepararé un plan de rutinas de ejercicios para
+                  cada semana 100% personalizado para realizar en casa o en el
+                  gimnasio, según necesites. Realizo el control y seguimiento de
+                  tu evolución de forma contínua y recibes la máxima asesoría
+                  para las dudas que tengas en cualquier aspecto de tu plan.
+                </p>
+                <button type="button">Saber más</button>
+              </div>
+              <div className="smallColumn darkGrey">
+                <h3>Dieta Personalizada y Asesoría Nutricional</h3>
+
+                <p>
+                  Recibirás además un plan de dieta cada mes para combinar con
+                  tus rutinas, adaptado totalmente a tus necesidades y con todos
+                  los detalles: peso, porcentajes de macros y calorías diarias,
+                  que te ayudará a complementar con tu entrenamiento para que
+                  puedas alcanzar tus objetivos físicos en el menor tiempo
+                  posible. Puedes estar tranquil@, ¡no pasarás hambre!
+                </p>
+                <button type="button">Saber más</button>
+              </div>
             </div>
           </div>
+        </div>
+        <div className="grid three">
+          <div className="beforeAfter">Before/After Picture</div>
+          <div className="beforeAfter">Before/After Picture</div>
+          <div className="beforeAfter">Before/After Picture</div>
+          <div className="beforeAfter">Before/After Picture</div>
+          <div className="beforeAfter">Before/After Picture</div>
+          <div className="beforeAfter">Before/After Picture</div>
         </div>
       </body>
     );
