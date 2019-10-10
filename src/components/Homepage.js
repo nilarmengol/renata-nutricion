75f99e35ecd08639b00462038906b4a5602c69fd
Nil <nilarmengol@gmail.com>
2019-10-10 19:06:52 +0700

    grid four

--- src/components/Homepage.js
+++ src/components/Homepage.js
@@ -66,9 +66,64 @@ class Homepage extends React.Component {
           <div className="beforeAfter">Before/After Picture</div>
           <div className="beforeAfter">Before/After Picture</div>
           <div className="beforeAfter">Before/After Picture</div>
+          <div className="beforeAfter">
+            Before/After Picture -- Add Carrousel
+          </div>
           <div className="beforeAfter">Before/After Picture</div>
           <div className="beforeAfter">Before/After Picture</div>
-          <div className="beforeAfter">Before/After Picture</div>
+        </div>
+        <h2 className="question">
+          ¿Como funciona mi servicio de Entrenamiento Personal Online?
+        </h2>
+        <div className="grid four">
+          <div className="red hi">
+            <i class="fas fa-bullseye"></i>
+            <h3>1. Definición de Objetivos</h3>
+            <p>
+              Una vez definidos los objetivos y tras suscribirte al servicio,
+              deberás rellenar el cuestionario inicial con el que conoceré tu
+              estado y nivel físico inicial, composición corporal, hábitos,
+              estilo de vida, historial de lesiones, y más factores, para poder
+              prepararte el plan de entrenamiento 100% personalizado que te
+              ayudará a alcanzar tu meta en el menor tiempo
+            </p>
+          </div>
+          <div className="red1">
+            <i class="far fa-copy"></i>
+
+            <h3>2. Rutinas de Entrenamiento + Dieta Mensual</h3>
+            <p>
+              En un plazo de 48 a 72 horas te enviaré un plan de entrenamiento a
+              medida para tu primer mes, compuesto por rutinas de ejercicios
+              para hacer en casa o en el gimnasio y con todo tipo de detalles y
+              explicaciones de ejecución en texto y visual y un plan de dieta a
+              medida que te ayudará a perder grasa y recuperar tu mejor forma
+              física. Podrás descargar todo y acceder desde móvil, tablet y PC.
+            </p>
+          </div>
+          <div className="red2">
+            <i class="fas fa-chart-bar"></i>
+            <h3>3. Asesoría y Seguimiento</h3>
+            <p>
+              Realizaré el seguimiento de tu evolución física cada 15 días y
+              realizaré las adaptaciones necesarias cada mes en el programa en
+              función de como evolucionas. Mídete, registra tus medidas y sube
+              tus fotografías. Te asesoro y respondo a todas las preguntas que
+              tengas siempre que lo necesites por el chat o email. ¡Busco
+              feedback!
+            </p>
+          </div>
+          <div className="red3">
+            <i class="far fa-heart"></i>
+            <h3>4. ¡Gana en Salud y Bienestar!</h3>
+            <p>
+              Este plan de entrenamiento está pensado principalmente para
+              mujeres que deseen recuperar su mejor forma física y perder grasa
+              de forma saludable en poco tiempo con un programa preparado a
+              medida, realizando ejercicio cuando y donde deseen, para ganar en
+              salud y bienestar
+            </p>
+          </div>
         </div>
       </body>
     );
